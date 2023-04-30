bd6b4483bcc4c6a7239508f57e26377e0d25dae5
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-04-30 11:16:58 +0530

    updated note

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Meet Kamlesh, an upcoming developer with a focus on Python, React, and
+        backend APIs.
+      </SectionText>
+    </LeftSection>
+  </section>
 );
 
 export default Hero;
